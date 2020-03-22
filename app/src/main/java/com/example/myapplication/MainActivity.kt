918c291a4c48c41d9eb1abdae0b73fdfb9a7a9bb
raulacubas <ra.acuna.bas@gmail.com>
2020-03-22 19:34:01 -0300

    primero

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         suma()
-
         resta()
         multi()
         division()
