eb05573df5c2d162ce43067a89b1fcabe9925a1d
raulacubas <ra.acuna.bas@gmail.com>
2020-03-22 19:11:33 -0300

    se crea espacio entre sumay resta

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         suma()
+
         resta()
         multi()
         division()
